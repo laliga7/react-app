cf50c3ca76add2cac8b4a45f13ff523e8f79b162
laliga7 <57897264+laliga7@users.noreply.github.com>
2022-05-04 21:51:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-import App from './App';
 import Login from './pages/login';
 import UserPage from './pages/userPage'
 import AdminPage from './pages/adminPage';
