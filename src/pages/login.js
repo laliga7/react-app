c038f18f37a9297941244a88c92975228e39a4d4
laliga7 <molefe.kgothatso7@gmail.com>
2022-05-04 22:34:19 +0200

    three

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "../css/signin.css"
+import "../css/Loginstyle.css"
 
 import { findUser } from '../functions/login';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
+import profile from '../assets/profile.jpg'
 
 function Login() {
 
@@ -59,22 +61,40 @@ function Login() {
     // const 
     return (
         
-        <main className="form-signin">
-            <div className='bodybody'>
-                <form className='signin' onSubmit={onSubmitHandler}>
-                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-                    <div className="form-floating">
-                    <input onChange={e=> setUser({...user,title:e.target.value})} type="text" className="form-control" id="floatingInput" placeholder="title" />
-                    <label htmlFor="floatingInput">Title</label>
-                    </div>
-                    <div className="form-floating">
-                    <input onChange={e=> setUser({...user,content:e.target.value})} type="text" className="form-control" id="floatingPassword" placeholder="Content" />
-                    <label htmlFor="floatingPassword">Content</label>
-                    </div>
-                    <button onClick={() => fetchUser(user.title)} className="w-100 btn btn-lg btn-primary">Sign in</button>
+        // <main className="form-signin">
+        //     <div className='bodybody'>
+        //         <form className='signin' onSubmit={onSubmitHandler}>
+        //             <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        //             <div className="form-floating">
+        //             <input onChange={e=> setUser({...user,title:e.target.value})} type="text" className="form-control" id="floatingInput" placeholder="title" />
+        //             <label htmlFor="floatingInput">Title</label>
+        //             </div>
+        //             <div className="form-floating">
+        //             <input onChange={e=> setUser({...user,content:e.target.value})} type="text" className="form-control" id="floatingPassword" placeholder="Content" />
+        //             <label htmlFor="floatingPassword">Content</label>
+        //             </div>
+        //             <button onClick={() => fetchUser(user.title)} className="w-100 btn btn-lg btn-primary">Sign in</button>
+        //         </form>
+        //     </div>
+        // </main>
+
+        <header>
+            <div className="loginbox">
+                <img src={profile} className="profile" alt="background_image"/>
+                <h1 style={{color: '#fff'}}>Login Here</h1>
+                <form onSubmit={onSubmitHandler}>
+                    <span>Username</span>
+                    <input onChange={e=> setUser({...user,title:e.target.value})} id="userNameInp" type="text" name="userName" placeholder="Enter Username" />
+                    <span>Password</span>
+                    <input onChange={e=> setUser({...user,content:e.target.value})} id="passwordInp" type="password" name="password" placeholder="Enter Password" />
+                    {/* <br /> */}
+                    <button id="loginBtn" onClick={() => fetchUser(user.title)}>Login</button>
+                {/*-----addd  ---------*/}
+                <br />
                 </form>
             </div>
-        </main>
+        </header>
+
     );
 }
 
